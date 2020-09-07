ea215d3cfa5ce97bc2c6b212c434a981e8fc3aa6
paula-morales <pmpoclava@gmail.com>
2020-09-07 15:20:32 +0200

    router to get the recipeId

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { RecipesService } from "../recipes.service";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: "app-recipe-detail",
@@ -6,7 +9,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-detail.page.scss"],
 })
 export class RecipeDetailPage implements OnInit {
-  constructor() {}
+  recipe: Recipe;
 
-  ngOnInit() {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipesService: RecipesService
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("recipeId")) {
+        //redirect
+        return;
+      }
+      const recipeId = paramMap.get("recipeId");
+      this.recipe = this.recipesService.getRecipe(recipeId);
+    });
+  }
 }

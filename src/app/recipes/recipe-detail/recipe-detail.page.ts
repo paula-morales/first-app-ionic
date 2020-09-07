d6c52d3ca3efd982751e7def25f159bcd55a5fbb
paula-morales <pmpoclava@gmail.com>
2020-09-07 14:54:04 +0200

    new recipe details page

---
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.page.html",
+  styleUrls: ["./recipe-detail.page.scss"],
+})
+export class RecipeDetailPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}

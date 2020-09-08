05f95326b9c32d862e19ecc8d6110b710cb87327
paula-morales <pmpoclava@gmail.com>
2020-09-08 14:34:37 +0200

    back button

--- src/app/places/search/place-detail/place-detail.page.ts
+++ src/app/places/search/place-detail/place-detail.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-place-detail',
-  templateUrl: './place-detail.page.html',
-  styleUrls: ['./place-detail.page.scss'],
+  selector: "app-place-detail",
+  templateUrl: "./place-detail.page.html",
+  styleUrls: ["./place-detail.page.scss"],
 })
 export class PlaceDetailPage implements OnInit {
+  constructor(private navCrl: NavController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  onBookPlace() {
+    this.navCrl.navigateBack("/places/tabs/search");
   }
-
 }
